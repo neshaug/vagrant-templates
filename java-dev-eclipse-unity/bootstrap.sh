96af222bd4c9f71ea43e6785b8a69facba027d9b
Øyvind Neshaug <oyvind@neshaug.net>
2013-04-16 09:44:55 +0200

    Fix Eclipse plugin installation

--- java-dev-eclipse-unity/bootstrap.sh
+++ java-dev-eclipse-unity/bootstrap.sh
@@ -40,6 +40,6 @@ echo "Installing Eclipse plugins. Please be patient.."
 sudo -u vagrant -s /home/vagrant/bin/eclipse \
 -application org.eclipse.equinox.p2.director -noSplash \
 -repository http://download.eclipse.org/releases/juno,\
-http://m2eclipse.sonatype.org/sites/m2e\
+http://m2eclipse.sonatype.org/sites/m2e \
 -installIUs org.eclipse.egit.feature.group,\
 org.eclipse.m2e.feature.feature.group
